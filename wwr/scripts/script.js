ea7a8f7864d53cfad85648bd51041a690b730bb4
trechds <trechbusiness@gmail.com>
2023-11-08 01:14:03 -0300

    Updating River Page 5

--- wwr/scripts/script.js
+++ wwr/scripts/script.js
@@ -1,16 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const images = ["images/facil.png", "images/medio.png", "images/dificil.png", "images/perigoso.png"]; // Adicione os nomes dos arquivos de imagem do seu slideshow
+    const images = ["images/facil.png", "images/medio.png", "images/dificil.png", "images/perigoso.png"];
     let currentImageIndex = 0;
     const riverImage = document.querySelector('.river-image');
     const prevSlide = document.querySelector('.prev-slide');
     const nextSlide = document.querySelector('.next-slide');
+    const indicatorsContainer = document.querySelector('.indicators');
 
     function showImage(index) {
-        riverImage.src = `images/${images[index]}`;
-        // Atualize outras informações conforme necessário (nome do rio, descrição, etc.)
+        riverImage.src = images[index];
+        updateIndicators(index);
+    }
+
+    function updateIndicators(index) {
+        const indicators = document.querySelectorAll('.indicator');
+        indicators.forEach((indicator, i) => {
+            indicator.classList.toggle('active-indicator', i === index);
+        });
+    }
+
+    function createIndicators() {
+        images.forEach((_, index) => {
+            const indicator = document.createElement('span');
+            indicator.classList.add('indicator');
+            indicator.addEventListener('click', () => {
+                showImage(index);
+            });
+            indicatorsContainer.appendChild(indicator);
+        });
+        updateIndicators(currentImageIndex);
     }
 
     showImage(currentImageIndex);
+    createIndicators();
 
     prevSlide.addEventListener('click', function() {
         currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
